6d04df5a8ae1b55244e325bba448d147c6afe687
Islamul-Hoque <islamulhoque2006@gmail.com>
2025-12-11 22:54:43 +0600

    feat(user-management-api): introduce user delete endpoint with admin access control

--- index.js
+++ index.js
@@ -229,11 +229,11 @@ async function run() {
         });
 
         // Delete user account (Admin only)
-        // app.delete('/users/:id', async (req, res) => {
-        //         const id = req.params.id;
-        //         const result = await userCollection.deleteOne({ _id: new ObjectId(id) });
-        //         res.send(result);
-        // });
+        app.delete('/users/:id', async (req, res) => {
+                const id = req.params.id;
+                const result = await userCollection.deleteOne({ _id: new ObjectId(id) });
+                res.send(result);
+        });
 
         // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
